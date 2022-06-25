fac9ab8dbc2f89a066e9c0d6f5c8cde105678213
ZSI <89996056+ZSIOffical@users.noreply.github.com>
2022-06-25 17:23:50 +0200

    Create de.lua

---
+++ de.lua
@@ -0,0 +1,33 @@
+local German = {
+    --› Buttons,etc
+    ['Antiadmin join'] = "Antiadmin beitreten",
+    ['Antiban'] = 'Antiban',
+    ['Block all Admins, etc'] = 'Alle Admins sperren, etc.',
+    ['Kill'] = "töten",
+    ['Enter a player name to kill'] = 'Geben Sie einen Spielernamen zum Töten ein',
+    ['Walkspeed'] = 'Gehgeschwindigkeit',
+    ['Jumppower'] = 'Sprungkraft',
+    ['Click-tp tool'] = 'Klick-TP-Werkzeug',
+    ['Join smallest server'] = 'Kleinstem Server beitreten',
+    ['Server hop'] = 'Server-Sprung',
+    ['Private Servers'] = 'Private Server',
+    ['TP to boxes'] = 'Zu den Kisten teleportieren',
+    ['Box Aura'] = 'Kisten Aura',
+    ['Auto sell'] = 'Automatisches verkaufen',
+    ['Spin Arcade'] = "Arkade drehen",
+    ['Time between box tp'] = 'Zeit zwischen den Boxen tp',
+    ['Time between Box Aura'] = 'Zeit zwischen Box Aura',
+    ['Time between AutoSell'] = "Zeit zwischen Automatisches verkaufen",
+    ['Sell'] = 'Verkaufen',
+    ['Buy'] = 'kaufen',
+    ['Teleport to player'] = 'Zum Spieler teleportieren',
+    ['Teleport to NPC"'] = 'Zum NPC teleportieren'
+    --› Sections/Tabs
+    ['Teleports'] = 'Teleports'
+    ['Buy & Sell'] = 'kaufen & verkaufen',
+    ['Autofarms'] = 'Automatische Farmen',
+    ['Server'] = 'Server',
+    ['LocalPlayer'] = "Lokaler Spieler",
+    ['Security'] = 'Sicherheit'
+}
+return German
